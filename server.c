4929f43a2c19af778cd0eedb2a045886118f73b6
youngsuhk <djfls0304@naver.com>
2017-11-26 22:24:48 +0900

    sever

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <curses.h>
+#include <string.h>
+
+int main(int ac,char *av[])
+{
+
+}
