ba64111651b3be669d6905b77d0098cbc8b5afff
youngsuhk <djfls0304@naver.com>
2017-11-26 22:25:17 +0900

    client

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <curses.h>
+#include <string.h>
+
+int main(int arge,char *argv[])
+{
+
+}
