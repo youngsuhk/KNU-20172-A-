27ef4dacdfa2566ae38a1ce2ee543b9bad775e8f
gruR <33712302+gruR@users.noreply.github.com>
2017-11-29 09:46:04 +0900

    Update server.c

--- server.c
+++ server.c
@@ -6,6 +6,24 @@
 #include <curses.h>
 #include <string.h>
 
+//player information
+struct information_p{
+  char id[5];
+  int num_p, win, time;
+};
+
+//client information
+sturct client_2{
+  int socket, player;
+  struct information_p info_p;
+  int num;
+};
+
+struct information_p info_p[2];
+
+void write_p(struct); // client info write
+void read_p(struct);
+
 int main(int ac,char *av[])
 {
 
